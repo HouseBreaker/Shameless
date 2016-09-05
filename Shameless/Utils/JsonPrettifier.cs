fc0c001507a8fb454a7f06542900816105516dd2
Housey <housebreaker1@gmail.com>
2016-09-05 23:27:11 +0300

    replace json prettifier with built in json.net method

--- Shameless/Utils/JsonPrettifier.cs
+++ Shameless/Utils/JsonPrettifier.cs
@@ -1,84 +1,14 @@
-﻿namespace Shameless.Utils
-{
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-	/// <summary>
-	/// Prettify JSON
-	/// Source: http://stackoverflow.com/a/6237866/5499609
-	/// </summary>
+namespace Shameless.Utils
+{
 	public static class JsonPrettifier
 	{
-		private const string INDENT_STRING = "\t";
 		public static string FormatJson(string str)
 		{
-			var indent = 0;
-			var quoted = false;
-			var sb = new StringBuilder();
-			for (var i = 0; i < str.Length; i++)
-			{
-				var ch = str[i];
-				switch (ch)
-				{
-					case '{':
-					case '[':
-						sb.Append(ch);
-						if (!quoted)
-						{
-							sb.AppendLine();
-							Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
-						}
-						break;
-					case '}':
-					case ']':
-						if (!quoted)
-						{
-							sb.AppendLine();
-							Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
-						}
-						sb.Append(ch);
-						break;
-					case '"':
-						sb.Append(ch);
-						bool escaped = false;
-						var index = i;
-						while (index > 0 && str[--index] == '\\')
-							escaped = !escaped;
-						if (!escaped)
-							quoted = !quoted;
-						break;
-					case ',':
-						sb.Append(ch);
-						if (!quoted)
-						{
-							sb.AppendLine();
-							Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
-						}
-						break;
-					case ':':
-						sb.Append(ch);
-						if (!quoted)
-							sb.Append(" ");
-						break;
-					default:
-						sb.Append(ch);
-						break;
-				}
-			}
-			return sb.ToString();
-		}
-	}
-
-	public static class JsonHelperExtensions
-	{
-		public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
-		{
-			foreach (var i in ie)
-			{
-				action(i);
-			}
+			var formatted = JArray.Parse(str).ToString(Formatting.Indented);
+			return formatted;
 		}
 	}
 }
